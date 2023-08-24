bdeb5be2cb760d009e270403ce716b3f5174990c
crashkz <info.nauruzbayev@gmail.com>
2023-08-24 14:04:19 +0600

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,38 @@
 import { createStore } from 'vuex'
 
+// web3 - npm version 1.8.2
+const Web3 = require('web3')
+const web3 = new Web3('wss://eth-goerli.g.alchemy.com/v2/XruToWBXmylHCyWPYvePYpPW7r_rmbWs')
+
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        blocks: []
+    },
+    getters: {
+    },
+    mutations: {
+        addBlock(state, newBlock){
+            state.blocks.unshift(newBlock)
+        }
+    },
+    actions: {
+        async newBlockHeaders({commit}){
+            let subscribe = web3.eth.subscribe("newBlockHeaders")
+            .on('data', block => {
+                let newBlock = {
+                    number: block.number,
+                    hash: block.hash
+                }
+                commit('addBlock', newBlock)
+            })
+        },
+        async getBlock({commit}, blockNumberOrHash){
+            return await web3.eth.getBlock(blockNumberOrHash)
+        },
+        async getTransaction({commit}, transactionHash){
+            return await web3.eth.getTransaction(transactionHash)
+        }
+    },
+    modules: {
+    }
+    })
